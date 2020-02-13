83c9591f4a69638634d656bc58d237f31411afd3
Eze Sunday Eze <mailsforeze@gmail.com>
2020-02-13 22:06:00 +0100

    initical commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+
+if(isset($_POST['lat'], $_POST['lng'])) {
+    $lat = $_POST['lat'];
+    $lng = $_POST['lng'];
+
+    $url = sprintf("https://maps.googleapis.com/maps/api/geocode/json?latlng=%s,%s", $lat, $lng);
+
+    $content = file_get_contents($url); // get json content
+
+    $metadata = json_decode($content, true); //json decoder
+
+    if(count($metadata['results']) > 0) {
+        // for format example look at url
+        // https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452
+        $result = $metadata['results'][0];
+
+        // save it in db for further use
+        echo $result['formatted_address'];
+
+    }
+    else {
+        // no results returned
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Geocoding Page</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script>
+  function getLocation() {
+      if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(savePosition, positionError, {timeout:10000});
+      } else {
+          //Geolocation is not supported by this browser
+      }
+  }
+
+  // handle the error here
+  function positionError(error) {
+      var errorCode = error.code;
+      var message = error.message;
+
+      alert(message);
+  }
+
+  function savePosition(position) {
+            $.post("geocoordinates.php", {lat: position.coords.latitude, lng: position.coords.longitude});
+  }
+  </script>
+</head>
+<body>
+    <button onclick="getLocation();">Get My Location</button>
+</body>
+</html>
